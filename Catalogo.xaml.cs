6ce7c007b7d9e1bb27e2dab17a066e1cba8205a1
Cristian Julian Munoz Buenahora <cristian.munoz.2020@upb.edu.co>
2020-11-30 19:35:07 -0500

    Agregar archivos de proyecto.

---
+++ Catalogo.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Books_Sumaary
+{
+    /// <summary>
+    /// Lógica de interacción para Catalogo.xaml
+    /// </summary>
+    public partial class Catalogo : Page
+    {
+        public Catalogo()
+        {
+            InitializeComponent();
+        }
+    }
+}
